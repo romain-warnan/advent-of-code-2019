3d8b26d52a9a51b167f15551276361a3b50f507a
Romain Warnan <romain.warnan@insee.fr>
2019-12-06 14:44:59 +0100

    Day06.

---
+++ src/test/java/fr/insee/aoc/days/Day06Test.java
@@ -0,0 +1,34 @@
+package fr.insee.aoc.days;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class Day06Test {
+    
+	private Day day = new Day06();
+	
+	@Test
+	public void case1_0() {
+		assertEquals("42", day.part1("src/test/resources/06-0.txt"));
+	}
+
+	@Test
+	public void case2_0() {
+		assertEquals("4", day.part2("src/test/resources/06-1.txt"));
+	}
+
+	@Test
+	public void part1() {
+		String answer = day.part1("src/main/resources/06.txt");
+		System.out.println(String.format("%s.1: %s", day.getClass().getSimpleName(), answer));
+		assertEquals("315757", answer);
+	}
+	
+	@Test
+	public void part2() {
+		String answer = day.part2("src/main/resources/06.txt");
+		System.out.println(String.format("%s.2: %s", day.getClass().getSimpleName(), answer));
+		assertEquals("481", answer);
+	}
+}
