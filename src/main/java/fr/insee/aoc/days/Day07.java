642fdcaa86d397bc230314de40ff8f302237316e
Romain Warnan <romain.warnan@gmail.com>
2019-12-07 09:42:40 +0100

    Day07.

---
+++ src/main/java/fr/insee/aoc/days/Day07.java
@@ -0,0 +1,4 @@
+package fr.insee.aoc.days;
+
+public class Day07 implements Day {
+}
---
+++ src/test/java/fr/insee/aoc/days/Day07Test.java
@@ -0,0 +1,41 @@
+package fr.insee.aoc.days;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class Day07Test {
+    
+	private Day day = new Day07();
+	
+	@Test
+	public void case1_0() {
+		assertEquals("43210", day.part1("src/test/resources/07-0.txt"));
+	}
+
+	@Test
+	public void case1_1() {
+		assertEquals("54321", day.part1("src/test/resources/07-1.txt"));
+	}
+
+
+	@Test
+	public void case1_2() {
+		assertEquals("65210", day.part1("src/test/resources/07-2.txt"));
+	}
+
+
+	@Test
+	public void part1() {
+		String answer = day.part1("src/main/resources/07.txt");
+		System.out.println(String.format("%s.1: %s", day.getClass().getSimpleName(), answer));
+		assertEquals("315757", answer);
+	}
+	
+	@Test
+	public void part2() {
+		String answer = day.part2("src/main/resources/07.txt");
+		System.out.println(String.format("%s.2: %s", day.getClass().getSimpleName(), answer));
+		assertEquals("481", answer);
+	}
+}
