cb39251c6f7c4bb45371cf466c6367dbc56a12ac
Romain Warnan <romain.warnan@gmail.com>
2019-12-07 22:59:57 +0100

    Day07.

--- src/main/java/fr/insee/aoc/days/Day07.java
+++ src/main/java/fr/insee/aoc/days/Day07.java
@@ -1,4 +1,29 @@
 package fr.insee.aoc.days;
 
+import fr.insee.aoc.cpu.IntCode;
+import fr.insee.aoc.utils.DayException;
+
+import static fr.insee.aoc.utils.Days.*;
+
 public class Day07 implements Day {
+
+    private static final int[] values = {0, 1, 2, 3, 4};
+
+    @Override
+    public String part1(String input, Object... params) {
+        int[] program = arrayOfInt(input, ",");
+        int max = permutations(values).stream()
+                .mapToInt(settings -> output(program, settings))
+                .max()
+                .orElseThrow(DayException::new);
+        return String.valueOf(max);
+    }
+
+    private static int output(int[] program, int[] settings) {
+        int output = 0;
+        for(int setting : settings) {
+            output = IntCode.withInputs(setting, output).execute(program);
+        }
+        return output;
+    }
 }
